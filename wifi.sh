c09ba8b6c662904664744cef96a76a6ced0b5617
macuyler <macuyler@gmail.com>
2019-12-15 16:54:07 -0600

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Install b43
-apt-get update
-apt-get install firmware-b43-installer
+sudo apt-get update
+sudo apt-get install firmware-b43-installer
+sudo apt autoremove
+
+modprobe -r b44 b43 b43legacy ssb brcmsmac bcma wl mac80211 cordic brcmutil
 
 ip link set wlp2s0 down >/dev/null 2>&1
 ip link set wlp2s0b1 down >/dev/null 2>&1
 
-modprobe -r cfg80211 brcmsmac cordic brcmutil bcma b44 b43legacy ssb
-modprobe b43
+sudo modprobe -r cfg80211
+sudo modprobe mac80211
+sudo modprobe b43
+
+sudo systemctl restart network-manager.service
 
